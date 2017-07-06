7c3e507bb7ebc8b975b87aea2331acef7ea66fb2
Polina Marchenko <polina.marchenko@yahoo.com>
2017-07-06 16:11:57 -0700

    Add login and signup functionality

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Meteor } from 'meteor/meteor';
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    }
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+
+    Meteor.loginWithPassword({email}, password, (err) => {
+      console.log('Login callback', err);
+    });
+
+  }
+
   render() {
     return (
       <div>
-        <p>Login component here</p>
+        <h2>Short Link</h2>
+
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="email" ref="email" name="email" placeholder="Email" />
+          <input type="password" ref="password" name="password" placeholder="Password" />
+          <button>Login</button>
+        </form>
         <Link to="/signup">Need an account?</Link>
       </div>
     )
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { Accounts } from 'meteor/accounts-base';
 
 export default class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    }
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+
+    Accounts.createUser({email, password}, (err) => {
+      console.log('Signup callback', err);
+    })
+    // this.setState({
+    //   error: 'Something went wrong'
+    // })
+  }
+
   render() {
     return (
       <div>
         <h2>Join Short Link</h2>
+
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="email" ref="email" name="email" placeholder="Email" />
+          <input type="password" ref="password" name="password" placeholder="Password" />
+          <button>Create account</button>
+        </form>
         <Link to="/">Have an account?</Link>
       </div>
     )
